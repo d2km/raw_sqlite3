8f29e6dc5eebe9d97ed661aef0964e547d5e2a58
Denis Korzunov <disya2@gmail.com>
2020-08-16 17:34:32 +1000

    raw_sqlite3: add type specs and doc strings

--- src/raw_sqlite3.erl
+++ src/raw_sqlite3.erl
@@ -2,7 +2,7 @@
 %%
 %% NOTE: Currently the interface of the module should NOT be considered stable
 %% since it's mostly a convenience, high-level functions. Users of the library
-%% are expected to use sqltite3_nif module directly to build their own
+%% are expected to use sqlite3_nif module directly to build their own
 %% high-level primitives since its interface is as stable as the SQLite C API
 %% and is thoroughly type-specced and covered by tests.
 -module(raw_sqlite3).
@@ -14,6 +14,7 @@
          prepare/2,
          bind/2,
          step/1,
+         reset/1,
          fetchall/1,
          q/2, q/3,
          exec/2, exec/3,
@@ -28,28 +29,232 @@
          err_to_atom/1,
          expand_error/1, expand_error/2]).
 
+-export_type([
+              utf8str/0,
+              sqlite3/0,
+              sqlite3_error_code/0,
+              sqlite3_stmt/0,
+              query_parameter/0
+             ]).
+
+%% @type utf8str(). A <code>iodata()</code> term which, when serialised, should
+%% form a valid UTF8-encoded character string.
+-type utf8str() :: iodata().
+
+%% @type sqlite3(). A database connection handle.
+-opaque sqlite3() :: sqlite3_nif:sqlite3().
+
+%% @type sqlite3_error_code(). <em>sqlite3</em> error codes as atoms.
+-type sqlite3_error_code() ::
+        'SQLITE_OK'                      |
+        'SQLITE_ERROR'                   |
+        'SQLITE_INTERNAL'                |
+        'SQLITE_PERM'                    |
+        'SQLITE_ABORT'                   |
+        'SQLITE_BUSY'                    |
+        'SQLITE_LOCKED'                  |
+        'SQLITE_NOMEM'                   |
+        'SQLITE_READONLY'                |
+        'SQLITE_INTERRUPT'               |
+        'SQLITE_IOERR'                   |
+        'SQLITE_CORRUPT'                 |
+        'SQLITE_NOTFOUND'                |
+        'SQLITE_FULL'                    |
+        'SQLITE_CANTOPEN'                |
+        'SQLITE_PROTOCOL'                |
+        'SQLITE_EMPTY'                   |
+        'SQLITE_SCHEMA'                  |
+        'SQLITE_TOOBIG'                  |
+        'SQLITE_CONSTRAINT'              |
+        'SQLITE_MISMATCH'                |
+        'SQLITE_MISUSE'                  |
+        'SQLITE_NOLFS'                   |
+        'SQLITE_AUTH'                    |
+        'SQLITE_FORMAT'                  |
+        'SQLITE_RANGE'                   |
+        'SQLITE_NOTADB'                  |
+        'SQLITE_NOTICE'                  |
+        'SQLITE_WARNING'                 |
+        'SQLITE_ROW'                     |
+        'SQLITE_DONE'                    |
+        'SQLITE_ERROR_MISSING_COLLSEQ'   |
+        'SQLITE_ERROR_RETRY'             |
+        'SQLITE_ERROR_SNAPSHOT'          |
+        'SQLITE_IOERR_READ'              |
+        'SQLITE_IOERR_SHORT_READ'        |
+        'SQLITE_IOERR_WRITE'             |
+        'SQLITE_IOERR_FSYNC'             |
+        'SQLITE_IOERR_DIR_FSYNC'         |
+        'SQLITE_IOERR_TRUNCATE'          |
+        'SQLITE_IOERR_FSTAT'             |
+        'SQLITE_IOERR_UNLOCK'            |
+        'SQLITE_IOERR_RDLOCK'            |
+        'SQLITE_IOERR_DELETE'            |
+        'SQLITE_IOERR_BLOCKED'           |
+        'SQLITE_IOERR_NOMEM'             |
+        'SQLITE_IOERR_ACCESS'            |
+        'SQLITE_IOERR_CHECKRESERVEDLOCK' |
+        'SQLITE_IOERR_LOCK'              |
+        'SQLITE_IOERR_CLOSE'             |
+        'SQLITE_IOERR_DIR_CLOSE'         |
+        'SQLITE_IOERR_SHMOPEN'           |
+        'SQLITE_IOERR_SHMSIZE'           |
+        'SQLITE_IOERR_SHMLOCK'           |
+        'SQLITE_IOERR_SHMMAP'            |
+        'SQLITE_IOERR_SEEK'              |
+        'SQLITE_IOERR_DELETE_NOENT'      |
+        'SQLITE_IOERR_MMAP'              |
+        'SQLITE_IOERR_GETTEMPPATH'       |
+        'SQLITE_IOERR_CONVPATH'          |
+        'SQLITE_IOERR_VNODE'             |
+        'SQLITE_IOERR_AUTH'              |
+        'SQLITE_IOERR_BEGIN_ATOMIC'      |
+        'SQLITE_IOERR_COMMIT_ATOMIC'     |
+        'SQLITE_IOERR_ROLLBACK_ATOMIC'   |
+        'SQLITE_IOERR_DATA'              |
+        'SQLITE_LOCKED_SHAREDCACHE'      |
+        'SQLITE_LOCKED_VTAB'             |
+        'SQLITE_BUSY_RECOVERY'           |
+        'SQLITE_BUSY_SNAPSHOT'           |
+        'SQLITE_BUSY_TIMEOUT'            |
+        'SQLITE_CANTOPEN_NOTEMPDIR'      |
+        'SQLITE_CANTOPEN_ISDIR'          |
+        'SQLITE_CANTOPEN_FULLPATH'       |
+        'SQLITE_CANTOPEN_CONVPATH'       |
+        'SQLITE_CANTOPEN_DIRTYWAL'       |
+        'SQLITE_CANTOPEN_SYMLINK'        |
+        'SQLITE_CORRUPT_VTAB'            |
+        'SQLITE_CORRUPT_SEQUENCE'        |
+        'SQLITE_CORRUPT_INDEX'           |
+        'SQLITE_READONLY_RECOVERY'       |
+        'SQLITE_READONLY_CANTLOCK'       |
+        'SQLITE_READONLY_ROLLBACK'       |
+        'SQLITE_READONLY_DBMOVED'        |
+        'SQLITE_READONLY_CANTINIT'       |
+        'SQLITE_READONLY_DIRECTORY'      |
+        'SQLITE_ABORT_ROLLBACK'          |
+        'SQLITE_CONSTRAINT_CHECK'        |
+        'SQLITE_CONSTRAINT_COMMITHOOK'   |
+        'SQLITE_CONSTRAINT_FOREIGNKEY'   |
+        'SQLITE_CONSTRAINT_FUNCTION'     |
+        'SQLITE_CONSTRAINT_NOTNULL'      |
+        'SQLITE_CONSTRAINT_PRIMARYKEY'   |
+        'SQLITE_CONSTRAINT_TRIGGER'      |
+        'SQLITE_CONSTRAINT_UNIQUE'       |
+        'SQLITE_CONSTRAINT_VTAB'         |
+        'SQLITE_CONSTRAINT_ROWID'        |
+        'SQLITE_CONSTRAINT_PINNED'       |
+        'SQLITE_NOTICE_RECOVER_WAL'      |
+        'SQLITE_NOTICE_RECOVER_ROLLBACK' |
+        'SQLITE_WARNING_AUTOINDEX'       |
+        'SQLITE_AUTH_USER'               |
+        'SQLITE_OK_LOAD_PERMANENTLY'     |
+        'SQLITE_OK_SYMLINK'              |
+        'unknown_error'.
+
+-spec open(DbFile) -> Result
+              when DbFile :: utf8str(),
+                   Result :: {ok, Db} | {error, Reason},
+                   Db     :: sqlite3(),
+                   Reason :: {sqlite3_error_code(), utf8str()}.
+%% @doc Open a new database connection.
 open(DbFile) ->
     open(DbFile, ?SQLITE_OPEN_READWRITE bor ?SQLITE_OPEN_CREATE).
 
+-spec open(DbFile, Flags) -> Result
+              when DbFile :: utf8str(),
+                   Flags  :: non_neg_integer(),
+                   Result :: {ok, Db} | {error, Reason},
+                   Db     :: sqlite3(),
+                   Reason :: {sqlite3_error_code(), utf8str()}.
+
+%% @doc Open a new database connection with the specified open flags.
+%% @param Flags is a combination of open flags <code>OR</code>-ed together.
+%% For example <code>?SQLITE_OPEN_READWRITE bor ?SQLITE_OPEN_CREATE</code>.
+%% @see sqlite3_nif:open_flag(). The list of possible flag values
 open(DbFile, Flags) ->
     open(DbFile, Flags, "").
 
+-spec open(DbFile, Flags, Vfs) -> Result
+              when DbFile :: utf8str(),
+                   Flags  :: non_neg_integer(),
+                   Vfs    :: utf8str(),
+                   Result :: {ok, Db} | {error, Reason},
+                   Db     :: sqlite3(),
+                   Reason :: {sqlite3_error_code(), utf8str()}.
+
+%% @doc Open a new database connection with the specified open flags and the VFS
+%% engine.
+%% See <a href="https://sqlite.org/c3ref/open.html">sqlite3 documentation</a>
+%% for details.
 open(DbFile, Flags, Vfs) ->
     expand_error(sqlite3_nif:sqlite3_open_v2(DbFile, Flags, Vfs)).
 
+-spec close(Db) -> Result
+              when Db     :: sqlite3(),
+                   Result :: sqlite3_error_code().
+%% @doc Close database connection.
+%% NOTE: Usually, it is not necessary to call the function because a garbage
+%% collection will destroy the connection object. However, it may be useful to
+%% have the explicit control over a connection handle in some situations.
 close(Db) ->
     expand_error(sqlite3_nif:sqlite3_close_v2(Db)).
 
+%% @type sqlite3_stmt(). A prepared SQL statement.
+-opaque sqlite3_stmt() :: sqlite3_nif:sqlite3_stmt().
+
+-spec prepare(Db, Sql) -> Result
+              when Db      :: sqlite3(),
+                   Sql     :: utf8str(),
+                   Result  :: {ok, Stmt} | {error, Reason},
+                   Reason  :: sqlite3_error_code()
+                            | {sqlite3_error_code(), ErrDesc},
+                   Stmt    :: sqlite3_stmt(),
+                   ErrDesc :: utf8str().
+%% @doc Prepare a statement to use with bind/2, step/1, fetchall/1, fetchall2.
 prepare(Db, Sql) ->
     expand_error(Db, sqlite3_nif:sqlite3_prepare_v2(Db, Sql)).
 
+-type query_parameter()  :: number()
+                          | true
+                          | false
+                          | nil
+                          | undefined
+                          | {text, utf8str()}
+                          | {blob, iodata()}.
 
+%% @doc Bind parameters to a prepared statement.
+-spec bind(Stmt, Params) -> Result
+              when Stmt    :: sqlite3_stmt(),
+                   Params  :: [query_parameter()],
+                   Result  :: ok | {error, {sqlite3_error_code(), ErrDesc}},
+                   ErrDesc :: utf8str().
 bind(Stmt, Params) ->
     expand_error(sqlite3_nif:sqlite3_bind(Stmt, Params)).
 
+-spec step(Stmt) -> Result
+              when Stmt    :: sqlite3_stmt(),
+                   Result  :: ok | Row | {error, {sqlite3_error_code(), ErrDesc}},
+                   ErrDesc :: utf8str(),
+                   Row     :: tuple().
+%% @doc Evaluate a prepared statement and fetch one result. A statement should
+%% be run till <code>ok</code> is returned.
+%% NOTE: a fully evaluated statement must be reset for re-use in another
+%% operation.
 step(Stmt) ->
     expand_error(sqlite3_nif:sqlite3_step(Stmt)).
 
+-spec reset(Stmt::sqlite3_stmt()) -> sqlite3_error_code().
+%% @doc Reset a prepared statement.
+reset(Stmt) ->
+    expand_error(sqlite3_nif:sqlite3_reset(Stmt)).
+
+-spec fetchall(Stmt) -> Result
+              when Stmt    :: sqlite3_stmt(),
+                   Result  :: Rows | {error, {sqlite3_error_code(), ErrDesc}},
+                   Rows    :: [tuple()],
+                   ErrDesc :: utf8str().
+%% @doc Evaluate a prepared statement and fetch all results.
 fetchall(Stmt) ->
     expand_error(fetchall(Stmt, [])).
 
@@ -63,9 +268,26 @@ fetchall(Stmt, Acc) ->
             fetchall(Stmt, [Row | Acc])
     end.
 
+-spec q(Db, Sql) -> Result
+              when Db      :: sqlite3(),
+                   Sql     :: utf8str(),
+                   Result  :: Rows | {error, {sqlite3_error_code(), ErrDesc}},
+                   Rows    :: [tuple()],
+                   ErrDesc :: utf8str().
+
+%% @doc Evaluate an SQL expression and fetch all results.
 q(Db, Sql) ->
     q(Db, Sql, []).
 
+-spec q(Db, Sql, Params) -> Result
+              when Db      :: sqlite3(),
+                   Sql     :: utf8str(),
+                   Params  :: [query_parameter()],
+                   Result  :: Rows | {error, {sqlite3_error_code(), ErrDesc}},
+                   Rows    :: [tuple()],
+                   ErrDesc :: utf8str().
+
+%% @doc Evaluate an SQL expression, binding the parameters, and fetch all results.
 q(Db, Sql, Params) ->
     case sqlite3_nif:sqlite3_prepare_v2(Db, Sql) of
         {ok, Stmt, _} ->
@@ -79,9 +301,30 @@ q(Db, Sql, Params) ->
             expand_error(Db, Err)
     end.
 
+-spec exec(Db, Sql) -> Result
+              when Db      :: sqlite3(),
+                   Sql     :: utf8str(),
+                   Result  :: ok | {error, Reason},
+                   Reason  :: sqlite3_error_code()
+                            | {sqlite3_error_code(), ErrDesc},
+                   ErrDesc :: utf8str().
+%% @doc Evaluate DDL/DML SQL statement(s).
 exec(Db, Sql) ->
     exec(Db, Sql, []).
 
+-spec exec(Db, Sql, Params) -> Result
+              when Db      :: sqlite3(),
+                   Sql     :: utf8str(),
+                   Params  :: [query_parameter()],
+                   Result  :: ok | {error, Reason},
+                   Reason  :: sqlite3_error_code()
+                            | {sqlite3_error_code(), ErrDesc},
+                   ErrDesc :: utf8str().
+
+%% @doc Evaluate a DDL/DML SQL statement, binding query parameters.
+%% NOTE: Using more than one SQL statement with non-empty parameter list will
+%% most likely result in an error because the function will try to bind the
+%% supplied parameter list to every statement.
 exec(_Db, <<>>, _Params) ->
     ok;
 exec(Db, <<$ , Rest/binary>>, Params) ->
@@ -105,6 +348,17 @@ exec(Db, Sql, Params) ->
             expand_error(Db, Err)
     end.
 
+-spec insert_many(Db, Sql, [Params]) -> Result
+              when Db      :: sqlite3(),
+                   Sql     :: utf8str(),
+                   Params  :: [query_parameter()],
+                   Result  :: ok | {error, Reason},
+                   Reason  :: sqlite3_error_code()
+                            | {sqlite3_error_code(), ErrDesc},
+                   ErrDesc :: utf8str().
+%% @doc Insert many values.
+%% The function prepares an DML SQL statement and binds/evaluates it to every
+%% element in <code>ParameterList</code>.
 insert_many(Db, Sql, ParameterList) ->
     case sqlite3_nif:sqlite3_prepare_v2(Db, Sql) of
         {ok, Stmt, _Leftover} ->
@@ -142,9 +396,43 @@ exec_one(Stmt, Params) ->
             Err
     end.
 
+-spec fold(Db, Sql, Fun, Acc0) -> Result
+              when Db      :: sqlite3(),
+                   Sql     :: utf8str(),
+                   Fun     :: fun((Elem, AccIn) -> AccOut),
+                   Elem    :: tuple(),
+                   Result  :: Acc | {error, Reason},
+                   Reason  :: sqlite3_error_code()
+                            | {sqlite3_error_code(), ErrDesc},
+                   Acc     :: term(),
+                   ErrDesc :: utf8str(),
+                   Acc0    :: Acc,
+                   AccIn   :: Acc,
+                   AccOut  :: Acc.
+
+%% @doc Calls <code>Fun(Elem, Acc)</code> on successive elements of the list
+%% formed by evaluating a SQL statement. <code>Fun/2</code> must return a new
+%% accumulator, which is passed to the next call. The function returns the
+%% final value of the accumulator or error.
 fold(Db, Sql, Fun, Acc) ->
     fold(Db, Sql, [], Fun, Acc).
 
+-spec fold(Db, Sql, Params, Fun, Acc0) -> Result
+              when Db      :: sqlite3(),
+                   Sql     :: utf8str(),
+                   Params  :: [query_parameter()],
+                   Fun     :: fun((Elem, AccIn) -> AccOut),
+                   Elem    :: tuple(),
+                   Result  :: Acc | {error, Reason},
+                   Reason  :: sqlite3_error_code()
+                            | {sqlite3_error_code(), ErrDesc},
+                   Acc     :: term(),
+                   ErrDesc :: utf8str(),
+                   Acc0    :: Acc,
+                   AccIn   :: Acc,
+                   AccOut  :: Acc.
+
+%% @doc Same as <code>fold/4</code> but accepts a parameter list.
 fold(Db, Sql, Params, Fun, Acc) ->
     case sqlite3_nif:sqlite3_prepare_v2(Db, Sql) of
         {ok, Stmt, _Leftover} ->
@@ -168,9 +456,31 @@ do_fold(Stmt, Fun, Acc) ->
                 do_fold(Stmt, Fun, Fun(Row, Acc))
         end.
 
+-spec map(Db, Sql, Fun) -> Result
+              when Db      :: sqlite3(),
+                   Sql     :: utf8str(),
+                   Fun     :: fun((tuple()) -> T),
+                   Result  :: [T] | {error, Reason},
+                   Reason  :: sqlite3_error_code()
+                            | {sqlite3_error_code(), ErrDesc},
+                   T       :: term(),
+                   ErrDesc :: utf8str().
+%% @doc Transform the result of evaluating a SQL query by applying
+%% <code>Fun(Elem)</code>. Returns the transformed list or an error.
 map(Db, Sql, Fun) ->
     map(Db, Sql, [], Fun).
 
+-spec map(Db, Sql, Params, Fun) -> Result
+              when Db      :: sqlite3(),
+                   Sql     :: utf8str(),
+                   Params  :: [query_parameter()],
+                   Fun     :: fun((tuple()) -> T),
+                   Result  :: [T] | {error, Reason},
+                   Reason  :: sqlite3_error_code()
+                            | {sqlite3_error_code(), ErrDesc},
+                   T       :: term(),
+                   ErrDesc :: utf8str().
+%% @doc Same as <code>map/4</code> but accepts a parameter list.
 map(Db, Sql, Params, Fun) ->
     case sqlite3_nif:sqlite3_prepare_v2(Db, Sql) of
         {ok, Stmt, _Leftover} ->
@@ -194,7 +504,14 @@ do_map(Stmt, Fun, Acc) ->
             do_map(Stmt, Fun, [Fun(Row) | Acc])
     end.
 
-
+-spec with_trxn(Db, F) -> Result
+              when Db :: sqlite3(),
+                   F  :: fun(() -> T),
+                   Result :: T | {error, M},
+                   T :: term(),
+                   M :: #{reason => term(), stacktrace => term()}.
+%% @doc Evaluate <code>F</code> in a transaction.
+%% The function automatically rolls back if <code>F</code> throws an error.
 with_trxn(Db, F) ->
     case exec(Db, "BEGIN") of
         ok ->
@@ -210,22 +527,29 @@ with_trxn(Db, F) ->
         {error, _} = Err -> Err
     end.
 
-
+%% @doc Convert a list of atoms into an integer flag.
+-spec make_flags([atom()]) -> integer().
 make_flags(Atoms) ->
     lists:foldl(fun(Flag, Acc) ->
                         atom_to_int_flag(Flag) bor Acc
                 end, 0, Atoms).
 
+%% @doc Convert boolean values into integers (SQlite uses 0 and 1 for boolean values).
+-spec bool_to_int(boolean()) -> 0 | 1.
 bool_to_int(true) ->
     1;
 bool_to_int(false) ->
     0.
 
+%% @doc Convert integer values into booleans (SQlite uses 0 and 1 for boolean values).
+-spec int_to_bool(integer()) -> boolean().
 int_to_bool(0) ->
     false;
 int_to_bool(X) when is_integer(X) ->
     true.
 
+%% @doc Convert an atom into an integer flag.
+-spec atom_to_int_flag(atom) -> integer().
 atom_to_int_flag('SQLITE_OPEN_READONLY') ->
     ?SQLITE_OPEN_READONLY;
 atom_to_int_flag('SQLITE_OPEN_READWRITE') ->
@@ -353,6 +677,8 @@ atom_to_int_flag('SQLITE_CHECKPOINT_RESTART') ->
 atom_to_int_flag('SQLITE_CHECKPOINT_TRUNCATE') ->
     ?SQLITE_CHECKPOINT_TRUNCATE.
 
+%% @doc Convert an integer error value into the corresponding error atom.
+-spec err_to_atom(integer()) -> atom().
 err_to_atom(?SQLITE_OK) ->
     'SQLITE_OK';
 err_to_atom(?SQLITE_ERROR) ->
@@ -564,6 +890,7 @@ err_to_atom(?SQLITE_OK_SYMLINK) ->
 err_to_atom(_) ->
     'unknown_error'.
 
+%% @doc Convert a sqlite3_nif error, if present, into an error atom.
 expand_error({ok, _Result} = OK) ->
     OK;
 expand_error({error, Err}) when is_integer(Err) ->
@@ -573,6 +900,8 @@ expand_error(Err) when is_integer(Err) ->
 expand_error(Otherwise) ->
     Otherwise.
 
+%% @doc Convert a sqlite3_nif error, if present, into an error atom and error
+%% description.
 expand_error(_Db, {ok, _Result} = OK) ->
     OK;
 expand_error(Db, {error, Err}) when is_integer(Err) ->

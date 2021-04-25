c9e6ad5357b2d6e197e1559ece43b1ba5041940e
Denis Korzunov <disya2@gmail.com>
2021-04-25 11:33:12 +1000

    Update SQLite to 3.35.5

--- test/happy_path_tests.erl
+++ test/happy_path_tests.erl
@@ -672,13 +672,13 @@ compileoption_test() ->
               end,
     [?assertEqual(1, sqlite3_compileoption_used(Opt)) || Opt <- OptList(0)].
 
--define(VSN, <<"3.35.4">>).
--define(VSN_NUM, 3035004).
+-define(VSN, <<"3.35.5">>).
+-define(VSN_NUM, 3035005).
 libversion_test() ->
     ?assertEqual(?VSN, sqlite3_libversion()),
     ?assertEqual(?VSN_NUM, sqlite3_libversion_number()).
 
--define(SOURCE_ID, <<"2021-04-02 15:20:15 5d4c65779dab868b285519b19e4cf9d451d50c6048f06f653aa701ec212df45e">>).
+-define(SOURCE_ID, <<"2021-04-19 18:32:05 1b256d97b553a9611efca188a3d995a2fff712759044ba480f9a0c9e98fae886">>).
 sourceid_test() ->
     ?assertEqual(?SOURCE_ID, sqlite3_sourceid()).
 
